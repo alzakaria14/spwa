6542f6fc1b81944d2342d69ff43528dc032472ca
alzakaria14 <119605355+alzakaria14@users.noreply.github.com>
2023-01-01 18:16:23 +0800

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -12,6 +12,13 @@ $database = 'db_namadatabase';
 
 $koneksi = mysqli_connect($host,$user,$password,$database);
 
+//variabel global costom programmer
+//--PERHATIAN! SETELAH APLIKASI SUDAH PRODUCTION JANGAN PERNAH MERUBAH SALT
+//JIKA SALT DIRUBAH MAKA SELURUH PASSWORD USER AKAN GAGAL DAN SALAH!
+//ARTINYA USER TIDAK BISA LOGIN!
+$salt = 'EtV&4dTVTZQmi$tQK4t90O&M4JkRwOq92$Iu8eThDfZ3F8tIu7';
+$is_created = date('Y-m-d H:i:s');
+
 //base-url
 function base_url($path){
     $base_url = 'http://localhost/nama-folder/'.$path;
