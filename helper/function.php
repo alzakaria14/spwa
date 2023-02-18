2c7ec069e76d8db5e4d927fcbd078a30c317a8dc
alzakaria14 <119605355+alzakaria14@users.noreply.github.com>
2023-02-18 18:33:39 +0800

    Add Some function
    random string and phone converter

--- helper/function.php
+++ helper/function.php
@@ -1,6 +1,7 @@
 <?php
 
-function generate_otp($n){
+function generate_otp($n)
+{
     $generator = '1357902468';
     $result = '';
     for($i = 0; $i < $n; $i++){
@@ -8,4 +9,33 @@ function generate_otp($n){
     }
 
     return $result;
+}
+
+function convert_phone_number($phoneNumber)
+{
+
+    $phoneNumber = preg_replace('/[^0-9-\s]/', '', $phoneNumber);
+
+    $phoneNumber = str_replace(array(' ', '-'), '', $phoneNumber);
+
+    if (substr($phoneNumber, 0, 1) === '0') {
+        $phoneNumber = substr($phoneNumber, 1);
+    }
+
+    $phoneNumber = '62' . $phoneNumber;
+
+    return $phoneNumber;
+}
+
+function random_string($length)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $characters_length = strlen($characters);
+    $random_string = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $random_string .= $characters[rand(0, $characters_length - 1)];
+    }
+
+    return $random_string;
 }
