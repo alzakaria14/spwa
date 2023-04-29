b6f3a3d8b083a593c263a513262391452af33405
alzakaria14 <119605355+alzakaria14@users.noreply.github.com>
2023-04-30 04:20:23 +0800

    Add coppyright

--- views/footer.php
+++ views/footer.php
@@ -1,3 +1,9 @@
+<footer class="bg-light text-center text-lg-start">
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2023 CV Ruang Karya Bersama. All rights reserved.
+    </div>
+</footer>
+
 <!-- Bootsrap JS -->
 <script src="assets/vendor/bootsrap-5.3/js/bootstrap.bundle.min.js"></script>
 <script src="assets/vendor/bootsrap-5.3/js/popper.min.js"></script>
