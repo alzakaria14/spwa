ee51051c5197d0af84e41ed82319c83f140b3c86
alzakaria14 <119605355+alzakaria14@users.noreply.github.com>
2023-08-12 15:30:06 +0800

    Update function.php

--- helper/function.php
+++ helper/function.php
@@ -107,3 +107,27 @@ function terbilang($nilai)
     }
     return $hasil;
 }
+
+function send_meeesage_telegram($chatId, $message, $botToken)
+{
+    $url = "https://api.telegram.org/bot$botToken/sendMessage";
+
+    $data = array(
+        'chat_id' => $chatId,
+        'text' => $message
+    );
+
+    $options = array(
+        'http' => array(
+            'method' => 'POST',
+            'header' => "Content-Type:application/x-www-form-urlencoded\r\n",
+            'content' => http_build_query($data)
+        )
+    );
+
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    return $result;
+}
+
